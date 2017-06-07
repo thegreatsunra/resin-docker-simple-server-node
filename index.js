66f5691df3f0dd7ce1bfe03b074e604dd77fcbf1
Dane Petersen <thegreatsunra@gmail.com>
2017-06-07 10:32:42 -0500

    Serve public folder

--- index.js
+++ index.js
@@ -1,13 +1,8 @@
 const express = require('express')
 const app = express()
 
-// reply to request with "Hello World!"
-app.get('/', (req, res) => {
-  res.send('Hello world!')
-})
+app.use(express.static('public'))
 
-//start a server on port 80 and log its start to our console
-const server = app.listen(80, () => {
-  const port = server.address().port
-  console.log('Example app listening on port ', port)
+app.listen(80, () => {
+  console.log('Express listening on port 80')
 })
